ccad2d923951773a849c7bdbb51c3d8745d88a9d
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2018-12-23 16:08:51 +0100

    Import App.js to Index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import config from './config';
-import './static/css/styles.css';
-import firebase from 'firebase';
-import LoginForm from './components/LoginForm';
+import App from './App';
 
-export default class App extends Component {
 
-    state = {
-        users: null
-    }
+ReactDOM.render(
+        <App />,
+    document.getElementById('root'));
 
-    componentDidMount() {
-        firebase.initializeApp(config);
 
-        const birdy = firebase.database().ref('users');
-        birdy.on('value', snap => {
-            this.setState({
-                users: snap.val()
-            })
-        });
-    }
-
-    render() {
-        return (
-            <LoginForm/>
-        )
-    }
-}
-
-ReactDOM.render(<App/>, document.getElementById('root'));
