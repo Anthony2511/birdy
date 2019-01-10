08134eeed2c5a0c5ba3cea3bb40eb63340167aa5
Anthony Beaumecker <anthonybeaumecker@gmail.com>
2019-01-10 12:10:33 +0100

    Login code

--- src/App.js
+++ src/App.js
@@ -3,31 +3,42 @@ import config from './config';
 import './static/css/styles.css';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import Home from "./components/Home";
 import RegisterForm from "./components/RegisterForm";
 
 class App extends Component {
-
-    state = {
-        users: null
+    constructor() {
+        super();
+        this.state = ({
+            user: null,
+        });
+        this.authListener = this.authListener.bind(this);
     }
-
     componentDidMount() {
         firebase.initializeApp(config);
+        this.authListener();
+    }
 
-        const birdy = firebase.database().ref('users');
-        birdy.on('value', snap => {
-            this.setState({
-                users: snap.val()
-            })
-        });
+    authListener() {
+        firebase.auth().onAuthStateChanged((user) => {
+                //console.log(user);
+                if (user) {
+                    this.setState({user});
+                    //localStorage.setItem('user', user.uid);
+                } else {
+                    this.setState({user: null});
+                    //localStorage.removeItem('user');
+                }
+            }
+        )
     }
 
     render() {
         return (
             <div>
-                <LoginForm/>
+                {this.state.user ? (<Home/>) : (<LoginForm/>)}
+
                 <div className="content">
                     <Switch>
                         <Route path="/register" component={RegisterForm}/>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,44 @@
 import React, {Component} from 'react';
 import Header from './Header';
 import Input from './Input';
+import firebase from 'firebase';
 import {NavLink} from 'react-router-dom';
 
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.signup = this.signup.bind(this);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    handleChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    login(e) {
+        e.preventDefault();
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    signup(e) {
+        e.preventDefault();
+        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u) => {
+        }).then((u) => {
+            console.log(u)
+        })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
 
     render() {
         return (
@@ -16,23 +50,34 @@ class LoginForm extends Component {
                         <div className="form__bloc">
                             <i className="form__input-username"></i>
                             <Input
-                                name="username"
-                                type="text"
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                                id="inputEmail"
+                                name="email"
+                                type="email"
                                 label="Adresse e-mail"
                                 placeholder="jeandupont@gmail.com"/>
                         </div>
                         <div className="form__bloc">
                             <i className="form__input-password"></i>
                             <Input
+                                value={this.state.password}
+                                onChange={this.handleChange}
                                 name="password"
-                                type="text"
+                                id="inputPassword"
+                                type="password"
                                 label="Mot de passe"
                                 placeholder="Entrez votre mot de passe"/>
                         </div>
-                        <Input
-                            type="submit"
-                            value="Se connecter"/>
-                        <NavLink to="/register" className="form__link">Pas encore inscrit ?</NavLink>
+                        <button type="submit"
+                                onClick={this.login}
+                                className="">
+                            Se connecter
+                        </button>
+                        <button onClick={this.signup}
+                                className="form__link">
+                            Pas encore inscrit ?
+                        </button>
                     </form>
                 </div>
             </React.Fragment>
